070e65ad0746534e304930f3768666ca5af5ba46
ヒシタニミク <167289643+kotorikottoT@users.noreply.github.com>
2024-05-29 17:22:50 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// JavaScript
+//console.log('Hello world!');
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  //direction: 'vertical',
+    spaceBetween: 30,
+    centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
